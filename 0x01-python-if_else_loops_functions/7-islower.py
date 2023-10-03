25ec7f3ab4c19f376b4759981c1a0e3ba2bc8ec5
Ahmed <ahmedelsharkwiti@gmail.com>
2023-10-03 20:39:54 +0300

    MM

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def islower(c):
-    if c != None:
-        if c >= 'a' and c <= 'z':
-            return True
-        else:
-            return False
+    if c == "":
+        raise ValueError
+    if c >= 'a' and c <= 'z':
+        return True
+    else:
+        return False
