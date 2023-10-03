3ddf8e704492c8f6795a7ea623208eb5f8ddcc7b
Ahmed <ahmedelsharkwiti@gmail.com>
2023-10-03 20:51:57 +0300

    MM

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    if str == "":
+        raise ValueError
+    for i in str:
+        if i >= 'a' and i <= 'z':
+            i = chr(ord(i) - 32)
+        print("{}".format(i), end="")
+    print()
+
