46b9f886f68e839598b4e74fa480d76dbc974995
Ahmed <ahmedelsharkwiti@gmail.com>
2023-10-09 17:40:32 +0300

    MM

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list == "":
+        return None
+    count = 1
+    for i in range(0, len(my_list)):
+        j = 0
+        j = i + 1
+        temp = my_list[i]
+        while j < len(my_list):
+            if temp < my_list[j]:
+                count = 0
+                break
+            else:
+                count = 1
+            j = j + 1
+        if count == 1:
+            break
+    return temp
