11e5dadfa1f834e3190fde22f5891c7212230a60
Ahmed <ahmedelsharkwiti@gmail.com>
2023-10-09 11:21:02 +0300

    MM

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,11 +2,12 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     i = 0
     j = 0
+    indcat = 0
     tuple_e = ()
     list_a = list(tuple_a)
     list_b = list(tuple_b)
     list_e = list(tuple_e)
-    while i < 2 and j < 2:
+    while indcat < 2:
         if i == len(list_a):
             c = 0
         else:
@@ -18,5 +19,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
             b = list_b[j]
             j = j + 1
         list_e.append(c + b)
+        indcat = indcat + 1
     tuple_e = tuple(list_e)
     return (tuple_e)
