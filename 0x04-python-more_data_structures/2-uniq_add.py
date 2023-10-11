3d41e30b1bf5021c57f0c05e96cc3026f0782a36
Ahmed <ahmedelsharkwiti@gmail.com>
2023-10-11 21:42:54 +0300

    MM

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,15 @@
+def uniq_add(my_list=[]):
+    new_list = []
+    sum = 0
+    indcat = 0
+    for i in my_list:
+        for n in new_list:
+            if n == i:
+                indcat = 1
+                break
+            else:
+                indcat = 0
+        if indcat == 0:
+            new_list.append(i)
+            sum += i
+    return sum
