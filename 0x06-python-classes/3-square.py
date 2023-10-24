d1a831f333384f0988b02445b8818efb1f87d96b
Ahmed <ahmedelsharkwiti@gmail.com>
2023-10-24 21:16:55 +0300

    MM

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+''' class Square has one attrupite which is the size of '''
+
+
+class Square:
+    '''function which will check if size int or not
+    and it will check if the size is less than 0
+    assign the value of size to size of the square '''
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    '''function which will return the value of area of the square'''
+    def area(self):
+        return self.__size * self.__size
