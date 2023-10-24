cc1434f3c969c536b0cb7cd5138df4011dfcd368
Ahmed <ahmedelsharkwiti@gmail.com>
2023-10-24 20:51:24 +0300

    MM

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+''' class Square has one attrupite which is the size of '''
+
+
+class Square:
+    '''function which will assign the value of number to size of the square'''
+    def __init__(self, number):
+        self.__size = number
