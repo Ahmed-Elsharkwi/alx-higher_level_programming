d834d7bc0e9a9b5c41cef30769c1a3e889bd7684
Ahmed <ahmedelsharkwiti@gmail.com>
2023-10-28 14:04:17 +0200

    MM

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
-def add_integer(a, b=98):
+"""
+add_integer is a module which will add two integer
+Both integers should be the type of int
+and both var should not be None
+if any varible is not integer our program will raise an error"""
 
-    if type(a) == float:
+
+def add_integer(a, b=98):
+    """ add integer is the function
+    which will add two integer to each other
+    """
+    if type(a) is float:
         a = int(a)
-    if type(b) == float:
+    if type(b) is float:
         b = int(b)
-    if (a is None and b is None) or (type(a) != int and type(b) != int):
-        raise TypeError("a must be an integer and b must be an integer")
-    if a is None or type(a) != int:
+    if a is None or type(a) is not int:
         raise TypeError("a must be an integer")
-    if b is None or type(b) != int:
+    if b is None or type(b) is not int:
         raise TypeError("b must be an integer")
     return a + b
--- 0x07-python-test_driven_development/add.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def add_integer(a, b=98):
-    return a + b
-print(add_integer(None, None))
