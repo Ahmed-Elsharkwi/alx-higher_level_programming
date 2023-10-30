612533b7684cab5e64082d1c2f0a6c229995e7ec
Ahmed <ahmedelsharkwiti@gmail.com>
2023-10-30 18:35:36 +0200

    MM

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,8 +11,8 @@ class Rectangle:
     """
     def __init__(self, width=0, height=0):
         """ set the width and height of the Rectangele"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
