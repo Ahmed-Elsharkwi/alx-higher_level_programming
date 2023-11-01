7e431b0c0b3fcd478c6dc3ee99ae37f87868a533
Ahmed <ahmedelsharkwiti@gmail.com>
2023-11-01 10:12:48 +0200

    MM

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -7,17 +7,25 @@ There should be no space at the beginning or at the end of each printed line
 """
 
 
-def text_indentation(text):
+def text_indentation(text=""):
     if type(text) is not str or text is None:
         raise TypeError("text must be a string")
     else:
-        li_text = text.split(" ")
-        for i in range(0, len(li_text)):
-            if '?' in li_text[i] or '.' in li_text[i] or ':' in li_text[i]:
-                print(li_text[i])
-                print()
-            else:
-                result = i + 1
-                print("{}".format(li_text[i]), end="")
-                if result != len(li_text):
-                    print(" ", end="")
+        if " " not in text:
+            for i in text:
+                if i in ['?', '.', ':']:
+                    print(i)
+                    print()
+                else:
+                    print(i, end="")
+        else:
+            li_text = text.replace("    ", " ").split(" ")
+            for i in range(0, len(li_text)):
+                if '?' in li_text[i] or '.' in li_text[i] or ':' in li_text[i]:
+                    print(li_text[i])
+                    print()
+                else:
+                    result = i + 1
+                    print("{}".format(li_text[i]), end="")
+                    if result != len(li_text):
+                        print(" ", end="")
