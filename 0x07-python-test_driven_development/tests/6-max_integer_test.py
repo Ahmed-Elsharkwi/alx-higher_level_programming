b82a4388742f5645f6eec75e35ee2eb022fe9e99
Ahmed <ahmedelsharkwiti@gmail.com>
2023-11-01 09:45:58 +0200

    MM

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,5 +6,9 @@ class Test_code(unittest.TestCase):
         self.assertEqual(max_integer([]), None)
     def test_list(self):
         self.assertEqual(max_integer([2, 3, 4, 5]), 5)
+        self.assertEqual(max_integer([5, 3, 4, 2]), 5)
+        self.assertEqual(max_integer([5, 3, 6, 1, 2]), 6)
+        self.assertEqual(max_integer([-1, 0, 2, 4, 5]), 5)
+        self.assertEqual(max_integer([5]), 5)
 if __name__ == '__main__':
     unittest.main()
