75c4766db959285e413825fce3686b627e2d2ee3
Ahmed <ahmedelsharkwiti@gmail.com>
2023-11-08 00:17:43 +0200

    MM

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+class student
+"""
+
+
+class Student:
+    """ class for each student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name, age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        that retrieves a dictionary
+        representation of a Student instance
+        """
+        is_okay = True
+        dic = {}
+        dict_1 = self.__dict__
+        if type(attrs) is list:
+            for word in attrs:
+                if type(word) is not str:
+                    is_okay = False
+        if type(attrs) is not list:
+            is_okay = False
+        if is_okay is True:
+            for word in attrs:
+                if word in dict_1:
+                    dic.update({word: dict_1[word]})
+            return dic
+        else:
+            return self.__dict__
