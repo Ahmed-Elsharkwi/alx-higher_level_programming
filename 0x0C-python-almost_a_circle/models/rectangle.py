bd4dcd126bdfd0003fb3ccd7e5a2e3630631ca3f
Ahmed <ahmedelsharkwiti@gmail.com>
2023-11-10 19:52:55 +0200

    MM

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python3
+"""
+Rectangle is an inherited class from Base class
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ Rectangle is an inherited class from Base class """
+    def check_width(width):
+        """ check the width """
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+
+    def check_height(height):
+        """ check the height """
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+
+    def check_x(x):
+        """ check x """
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+
+    def check_y(y):
+        """ check x """
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ 
+        assign all of those argrument to thier private attributes
+        except id is a puplic attribute
+        """
+        Rectangle.check_width(width)
+        Rectangle.check_height(height)
+        Rectangle.check_x(x)
+        Rectangle.check_y(y)
+
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """get the width """
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """ set width """
+        Rectangle.check_width(width)
+        self.__width = width
+
+    @property
+    def height(self):
+        """ get the height """
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """ set the height """
+        Rectangle.check_height(height)
+        self.__height = height
+
+    @property
+    def x(self):
+        """ get the x """
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        """ set the x """
+        Rectangle.check_x(x)
+        self.__x = x
+
+    @property
+    def y(self):
+        """ get the y """
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        """ set the y """
+        Rectangle.check_y(y)
+        self.__y = y
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python3
+""" test the first module """
+import unittest
+from models.rectangle import Rectangle
+
+
+class TestStringMethods(unittest.TestCase):
+    """ Test some methods using unittest """
+    def test_constructure_getter(self):
+        """ 
+        pass values through constructure 
+        and get the values using getter
+        """
+        obj = Rectangle(1, 2, 5, 8, 12)
+        self.assertEqual(obj.id, 12)
+        self.assertEqual(obj.width, 1)
+        self.assertEqual(obj.height, 2)
+        self.assertEqual(obj.x, 5)
+        self.assertEqual(obj.y, 8)
+
+    def test_setter_getter(self):
+        """pass values with setter and get it with getter"""
+        obj = Rectangle(9, 8)
+        self.assertEqual(obj.id, 1)
+        self.assertEqual(obj.width, 9)
+        self.assertEqual(obj.height, 8)
+        self.assertEqual(obj.x, 0)
+        self.assertEqual(obj.y, 0)
+
+        obj.width = 10
+        self.assertEqual(obj.width, 10)
+
+        obj.height = 20
+        self.assertEqual(obj.height, 20)
+
+        obj.x = 2
+        self.assertEqual(obj.x, 2)
+
+        obj.y = 4
+        self.assertEqual(obj.y, 4)
+
+    def test_type_error(self):
+        """ test the typeError for all variables """
+        with self.assertRaises(TypeError) as exc:
+            obj = Rectangle("Ahmed", 2, 3, 4, 12)
+            obj.width = "MO"
+            obj.width = [1, 2, 3]
+            obj.width = (1, 2, 3)
+            obj.width = {"key": 1, "key_2": 3}
+            obj.width = None
+        self.assertEqual(str(exc.exception), "width must be an integer")
+
+        with self.assertRaises(TypeError) as exc:
+            obj = Rectangle(2, "Ahmed", 3, 4, 12)
+            obj.height = "MO"
+            obj.height = [1, 2, 3]
+            obj.height = (1, 2, 3)
+            obj.height = {"key": 1, "key_2": 3}
+            obj.height = None
+        self.assertEqual(str(exc.exception), "height must be an integer")
+
+        with self.assertRaises(TypeError) as exc:
+            obj = Rectangle(3, 2, "Ahmed", 4, 12)
+            obj.x = "MO"
+            obj.x = [1, 2, 3]
+            obj.x = (1, 2, 3)
+            obj.x = {"key": 1, "key_2": 3}
+            obj.x = None
+        self.assertEqual(str(exc.exception), "x must be an integer")
+
+        with self.assertRaises(TypeError) as exc:
+            obj = Rectangle(3, 2, 4, "Ahmed", 12)
+            obj.y = "MO"
+            obj.y = [1, 2, 3]
+            obj.y = (1, 2, 3)
+            obj.y = {"key": 1, "key_2": 3}
+            obj.y = None
+        self.assertEqual(str(exc.exception), "y must be an integer")
+
+    def test_Value_Error(self):
+        """ test the valueError for all variables """
+        with self.assertRaises(ValueError) as exc:
+            obj = Rectangle(-1, 2, 4, 5, 12)
+            obj.width = 0
+        self.assertEqual(str(exc.exception), "width must be > 0")
+
+        with self.assertRaises(ValueError) as exc:
+            obj = Rectangle(1, -2, 4, 5, 12)
+            obj.height = 0
+        self.assertEqual(str(exc.exception), "height must be > 0")
+
+        with self.assertRaises(ValueError) as exc:
+            obj = Rectangle(1, 2, -4, 5, 12)
+            obj.x = 0
+        self.assertEqual(str(exc.exception), "x must be >= 0")
+
+        with self.assertRaises(ValueError) as exc:
+            obj = Rectangle(1, 2, 4, -5, 12)
+            obj.y = 0
+        self.assertEqual(str(exc.exception), "y must be >= 0")
