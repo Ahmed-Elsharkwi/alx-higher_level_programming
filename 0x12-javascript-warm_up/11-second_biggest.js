574f9204cb812c68122eb46e86bc5d32b3fab7c6
Ahmed <ahmedelsharkwiti@gmail.com>
2024-01-09 01:08:08 +0200

    MM

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,6 @@ if (args.length === 2 || args.length === 3) {
 } else {
   const arr = args.slice(2);
   const num = Math.max.apply(null, arr);
-  console.log(num);
   let number = arr[0];
   for (let j = 1; j < arr.length; j++) {
     if (number < num && number < arr[j]) {
