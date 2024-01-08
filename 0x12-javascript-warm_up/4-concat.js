6876aff00eefcc7f7d4c14a52ea5794d2308aa86
Ahmed <ahmedelsharkwiti@gmail.com>
2024-01-08 23:10:39 +0200

    MM

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
 const args = process.argv;
 
-if (args[2] == null) {
-  console.log('No argument');
-} else {
-  console.log(args[2] + ' is ' + args[3]);
-}
+console.log(args[2] + ' is ' + args[3]);
