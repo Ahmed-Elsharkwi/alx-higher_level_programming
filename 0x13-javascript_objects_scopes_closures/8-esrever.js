2b1cffd0a685c27cb5f3bf451e762393f08e6aae
Ahmed <ahmedelsharkwiti@gmail.com>
2024-01-09 19:13:33 +0200

    MM

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   const second = [];
-  for (let i = list.legth; i >= 0; i++) {
+  for (let i = list.length - 1; i >= 0; i--) {
     second.push(list[i]);
   }
   return (second);
