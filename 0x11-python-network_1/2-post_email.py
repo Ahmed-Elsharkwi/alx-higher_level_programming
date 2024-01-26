8f840899360d802eea929f3f90f5c1504bc62df8
Ahmed <ahmedelsharkwiti@gmail.com>
2024-01-27 00:28:56 +0200

    MM

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,6 +4,7 @@ if __name__ == "__main__":
     import urllib.request
     import sys
     data = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(data).encode("ascii")
     req = urllib.request.Request(sys.argv[1], data)
     with urllib.request.urlopen(req) as response:
         the_page = response.read().decode("utf-8")
