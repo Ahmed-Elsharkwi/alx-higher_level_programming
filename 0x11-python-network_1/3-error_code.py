367dbaac350f1bd0cfdcb83fe3c896b445754d2a
Ahmed <ahmedelsharkwiti@gmail.com>
2024-01-27 00:40:39 +0200

    MM

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" script that fetches https://alx-intranet.hbtn.io/status """
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            the_page = response.read().decode("utf-8")
+        print(the_page)
+    except urllib.error.HTTPError as e:
+        print("Error: {}".format(e.status))
