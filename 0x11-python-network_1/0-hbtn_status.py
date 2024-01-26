5e11d26f18ada0fb355e0f4e16194eb73b3e6c53
Ahmed <ahmedelsharkwiti@gmail.com>
2024-01-26 23:28:19 +0200

    add the script with will fetch the website of alx

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,6 +5,7 @@ import urllib.request
 req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
 with urllib.request.urlopen(req) as response:
     the_page = response.read()
-print("- type: {}".format(type(the_page)))
-print("- content: {}".format(the_page))
-print("- utf8 content: {}".format(the_page.decode("utf-8")))
+print("Body response:")
+print("     - type: {}".format(type(the_page)))
+print("     - content: {}".format(the_page))
+print("     - utf8 content: {}".format(the_page.decode("utf-8")))
