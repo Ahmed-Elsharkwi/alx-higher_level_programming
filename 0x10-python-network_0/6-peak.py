0d66c883d117c17f5d2231a06a1c463136933f17
Ahmed <ahmedelsharkwiti@gmail.com>
2024-01-26 19:41:53 +0200

    MM

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,9 +9,9 @@ def find_peak(list_of_integers):
     num = 0
     for i in range(len(list_of_integers)):
         if (list_of_integers[i - 1] is None or list_of_integers[
-            i] > list_of_integers[i - 1]):
+                i] > list_of_integers[i - 1]):
             if (i + 1 == len(list_of_integers) or list_of_integers[
-                i] > list_of_integers[i + 1]):
+                    i] > list_of_integers[i + 1]):
                 num = list_of_integers[i]
                 break
     if num == 0:
