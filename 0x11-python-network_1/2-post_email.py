bf9cf5bffe5c5917dfef1893b719914a8a20242b
Ahmed <ahmedelsharkwiti@gmail.com>
2024-01-27 00:23:41 +0200

    MM

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" script that fetches https://alx-intranet.hbtn.io/status """
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+    data = {"email":sys.argv[2]}
+    data = urllib.parse.urlencode(data).encode("ascii")
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read().decode("utf-8")
+    print(the_page)
