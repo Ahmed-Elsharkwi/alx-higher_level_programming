aad8839fb60cd1de6c28d3220addf8b04a64401d
Ahmed <ahmedelsharkwiti@gmail.com>
2024-01-27 21:42:44 +0200

    MM

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-""" 
-script that takes in a letter and 
-sends a POST request to http://0.0.0.0:5000/search_user with the 
+"""
+script that takes in a letter and
+sends a POST request to http://0.0.0.0:5000/search_user with the
 letter as a parameter.
 """
 if __name__ == "__main__":
     import requests
     import sys
 
-    da={"q": ""}
+    da = {"q": ""}
     if len(sys.argv) == 2:
         da = {"q": sys.argv[1]}
     r = requests.post(" http://0.0.0.0:5000/search_user", data=da)
-    
+
     try:
         data = r.json()
         if data == {}:
