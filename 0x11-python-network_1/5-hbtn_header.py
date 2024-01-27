ca922333fbb1f43460d6736c40d36e047887f353
Ahmed <ahmedelsharkwiti@gmail.com>
2024-01-27 22:11:05 +0200

    get the header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+script that takes in a letter and
+sends a POST request to http://0.0.0.0:5000/search_user with the
+letter as a parameter.
+"""
+if __name__ == "__main__":
+    import requests
+    import sys
+    r = requests.get(sys.argv[1])
+    print(r.headers["X-Request-Id"])
