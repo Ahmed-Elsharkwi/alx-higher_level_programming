f5ae2f72ebd8b1b9fa05467f3b45186b11682f2d
Ahmed <ahmedelsharkwiti@gmail.com>
2024-01-27 22:23:03 +0200

    fetech a websit

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -8,4 +8,5 @@ if __name__ == "__main__":
     import requests
     import sys
     r = requests.get(sys.argv[1])
-    print(r.headers.get("X-Request-Id"))
+    data = r.headers
+    print(data["X-Request-Id"])
