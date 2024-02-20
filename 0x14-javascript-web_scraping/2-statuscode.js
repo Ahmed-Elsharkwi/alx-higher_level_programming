87f5eee880b71c74afdf8d870dba8cbb5141fb61
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-02-20 21:31:07 +0200

    MM

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
-request(process.argv[2], function (response) {
-  console.log('code:', response && response.statusCode);
+request.get(process.argv[2], function (err, res) {
+  if (err) {
+    console.error('An error occurred:', err);
+    return;
+  }
+  console.log('code:', res.statusCode);
 });
