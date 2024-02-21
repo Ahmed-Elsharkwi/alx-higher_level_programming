806e94d951cc0db3faaad81e150ce2e29e483cd0
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-02-21 21:26:24 +0200

    MM

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -10,12 +10,13 @@ request.get(process.argv[2], function (err, res, body) {
   let count = 0;
   let user_id = data[0].userId;
   for (let i = 0; i < data.length; i++) {
-	  if (data[i].completed == true) {
-	       if (data[i].userId == user_id) {
+	  if (data[i].completed === true) {
+	       if (data[i].userId === user_id) {
 		       newdict[data[i].userId] = ++count;
 	      } else if (data[i].userId != user_id) {
 		       user_id = data[i].userId;
 		       count = 1;
+		       newdict[data[i].userId] = count;
 	      }
     }
   }
